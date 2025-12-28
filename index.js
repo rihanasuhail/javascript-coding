0d4f126aac4c1f7abbda447bacff923894a9e236
Rihana Suhail <Rihana.Suhail@ibm.com>
2025-12-28 12:32:19 +0400

    check palindrom

--- index.js
+++ index.js
@@ -6,4 +6,16 @@ function reverseString(str) {
     return revString
 }
 
+function checkPalindrome(str) {
+    let revString = ""
+    for (let i=str.length - 1; i>=0; i--) {
+        revString = revString + str[i]
+    }
+    if (revString === str){ return true }
+    else { return false}
+}
+
 console.log(reverseString("Hello there"))
+console.log(checkPalindrome("Hello there"))
+console.log(checkPalindrome("level"))
+console.log(checkPalindrome("madam"))
