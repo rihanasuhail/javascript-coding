f74683465fc99e389f30ea88443b515611df3f48
Rihana Suhail <Rihana.Suhail@ibm.com>
2026-01-01 22:10:49 +0400

    Anagram check

--- level2.js
+++ level2.js
@@ -4,16 +4,33 @@
 // Sorting in descending is b-a
 // arr.sort((a,b)=> b-a)
 // By default it is ascending
-function findSecondLargest(arr){
-    // arr to be turned into a new set to avoid duplication
+function findSecondLargest(arr) {
+  // arr to be turned into a new set to avoid duplication
 
-    const setArr = [...new Set(arr)]
-    const arrSorted = setArr.sort((a,b)=>b-a)
-    console.log(arrSorted[1])
+  const setArr = [...new Set(arr)];
+  const arrSorted = setArr.sort((a, b) => b - a);
+  console.log(arrSorted[1]);
 }
 
-findSecondLargest([1, 2, 3, 4, 5])
-findSecondLargest([5, 4, 3, 2, 1])
-findSecondLargest([10, 20, 30])
-findSecondLargest([7, 7, 5])
-findSecondLargest([3, 1, 2])
+function anagramCheck(str1, str2) {
+  const sortedstr1 = str1.toLowerCase().split("").sort().join(""); // join() function joins the contents with the seperator as argument
+  const sortedstr2 = str2.toLowerCase().split("").sort().join("");
+  if (sortedstr1 === sortedstr2) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+}
+findSecondLargest([1, 2, 3, 4, 5]);
+findSecondLargest([5, 4, 3, 2, 1]);
+findSecondLargest([10, 20, 30]);
+findSecondLargest([7, 7, 5]);
+findSecondLargest([3, 1, 2]);
+
+anagramCheck("gum", "mug");
+anagramCheck("listen", "silent");
+anagramCheck("evil", "vile");
+anagramCheck("race", "care");
+anagramCheck("hello", "world");
+anagramCheck("aab", "aba");
+anagramCheck("Listen", "Silent");
