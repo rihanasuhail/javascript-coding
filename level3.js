53a487029bd903880beca86be0bae6640dbd632b
Rihana Suhail <Rihana.Suhail@ibm.com>
2026-01-03 10:32:06 +0400

    Function is called only once

--- level3.js
+++ level3.js
@@ -20,3 +20,20 @@ function createCounter() {
 const countA = createCounter(); // this will return a function
 console.log(countA()); // it will keep the value of count in memory due to closure and increment it each time it's called
 console.log(countA());
+
+function calledOnce(fn) {
+  // here fn = (a,b)=> a+b
+  let calledAlready = false;
+  return function (...args) {
+    // here any number of arguments are taken in
+    if (!calledAlready) {
+      calledAlready = true;
+      return fn(...args);
+      // here the required number of arguments as per fn function is taken as argument
+    }
+  };
+}
+
+const calling1 = calledOnce((a, b) => a + b);
+console.log(calling1(2, 3));
+console.log(calling1(4, 5)); // wont return anything as the previous value is remembered due to closure
