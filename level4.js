cf7dee1d7334880bd3aa9ad5c0e0668f85c8f2a7
Rihana Suhail <Rihana.Suhail@ibm.com>
2026-01-04 15:16:43 +0400

    Retry failed promises

--- level4.js
+++ level4.js
@@ -21,3 +21,15 @@ async function fetchData(url) {
   console.log(data);
 }
 fetchData("https://jsonplaceholder.typicode.com/users");
+
+async function retryPromise(fn, retries) {
+  for (let i = 0; i < retries; i++) {
+    try {
+      return await fn();
+    } catch (error) {
+      if (i === retries - 1) {
+        throw error;
+      }
+    }
+  }
+}
